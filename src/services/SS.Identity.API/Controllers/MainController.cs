b2e4aee31d2085fd034a2b3d043376eb152ac113
Alexandre Freitas <alexandre.freitas@mouradubeux.com.br>
2023-04-06 15:11:04 -0300

    Adding a new custom MainController who will be used as default by the other controllers

---
+++ src/services/SS.Identity.API/Controllers/MainController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace SS.Identity.API.Controllers
+{
+    [ApiController]
+    public abstract class MainController : Controller
+    {
+        protected ICollection<string> Errors = new List<string>();
+
+        protected IActionResult CustomResponse(object result = null)
+        {
+            if (ProcessIsValid())
+                return Ok(result);
+
+            return BadRequest(new ValidationProblemDetails(new Dictionary<string, string[]>
+            {
+                { "Messages", Errors.ToArray() }
+            }));
+        }
+
+        protected IActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            var errors = modelState.Values.SelectMany(e => e.Errors);
+
+            foreach (var error in errors)
+                AddProcessingError(error.ErrorMessage);
+
+            return CustomResponse();
+        }
+
+        protected bool ProcessIsValid() 
+        {
+            return !Errors.Any();
+        }
+
+        protected void AddProcessingError(string error) 
+        {
+            Errors.Add(error);
+        }
+
+        protected void ClearProcessingErrors()
+        {
+            Errors.Clear();
+        }
+    }
+}
